c0b4021800e7e91be2e9907e882d0bba8ea3ec2d
Tom Planche <Tomplanche@icloud.com>
2025-04-21 03:31:17 +0200

    [6] (feat on main) Added fish completion for hooksmith.

---
+++ completions/hooksmith.fish
@@ -0,0 +1,8 @@
+# Basic command options
+complete -c hooksmith -s h -l help -d "Show help"
+complete -c hooksmith -s v -l verbose -d "Enable verbose output"
+complete -c hooksmith -s c -l config-path -d "Path to the hooksmith.yaml file"
+
+# Subcommands
+complete -c hooksmith -n "__fish_use_subcommand" -a "install" -d "Install all hooks listed in the config file"
+complete -c hooksmith -n "__fish_use_subcommand" -a "run" -d "Run a specific hook"
